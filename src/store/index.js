e83e64a30df4740aa303864d5b1ffaa22033aa28
KomanDante999 <Dreftware@yandex.ru>
2023-06-14 21:20:08 +0700

    getters

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,26 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import product from '@/data/products';
+import products from '@/data/products';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    cartProducts: []
+    cartProducts: [
+      {productId: 5, amount: 2},
+      {productId: 7, amount: 10},
+      {productId: 3, amount: 1},
+    ]
   },
   mutations: {
     addProductToCard(state, {productId, amount}){
       const item = state.cartProducts.find(item => item.productId === productId)
-
       if (item) {
         item.amount += amount
       } else {
         state.cartProducts.push({productId, amount})
       }
-
     },
     // addProductToCard(state, payload){
     //   state.cartProducts.push({
@@ -24,6 +28,19 @@ export default new Vuex.Store({
     //     amount: payload.amount
     //   })
     // },
+  },
+  getters: {
+    cartDetaiProducts(state){
+      return state.cartProducts.map(item => {
+        return {
+          ...item,
+          product: products.find(p => p.id === item.productId)
+        }
+      })
+    },
+    cartTotalPrise(state, getters){
+      return getters.cartDetaiProducts.reduce((acc, item) => (item.product.prise * item.amount) + acc, 1)
+    }
   }
 });
 
