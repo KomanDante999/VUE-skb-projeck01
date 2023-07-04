2df5294075cb4dd1e2ccf360a1cb2407f1000e04
komandante999 <dreftware@yandex.ru>
2023-07-04 09:37:37 +0700

    deploy

---
+++ deploy.js
@@ -0,0 +1,29 @@
+const { execSync } = require('child_process')
+try {
+  // Останавливаем сборку при ошибках
+  // execSync('set -e');
+
+  // Собираем проект
+  execSync('npm run build');
+
+  // Переходим в папку dist
+  execSync('cd dist');
+
+  // Инициализируем Git репозиторий
+  execSync('git init');
+
+  // Добавляем все файлы в коммит
+  execSync('git add -A');
+
+  // Создаем коммит
+  execSync('git commit -m "deploy"');
+
+  // Публикуем проект на gh-pages
+  execSync('git push -f https://github.com/KomanDante999/vue-tehnozavr.git master:gh-pages');
+
+  // Возвращаемся в исходную папку
+  execSync('cd -');
+} catch (error) {
+  console.error(`Ошибка при выполнении развертывания: ${error}`);
+  process.exit(1);
+}
